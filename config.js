1bf41373b1f19866111b886686d912f8752ce740
Asᴍᴏᴅᴇᴜs Eᴘᴢɪ 💸 <99525582+HaCkr-EPZI-public@users.noreply.github.com>
2024-11-19 20:44:10 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "",
+AUTO_VOICE: process.env.AUTO_VOICE || "true",
+AUTO_STICKER: process.env.AUTO_STICKER || "false",
+AUTO_REPLY: process.env.AUTO_REPLY || "true",
+};
